192a62579b12ccdfcdc9984c3898ddab1413afaf
kononov <kononov@>
2024-12-31 04:58:26 +0300

    DAY-5 | MOVE IMAGE IN CARD TO RIGHT AND LEFT. OPTIMIZE CARDS

--- components/icon/icon.py
+++ components/icon/icon.py
@@ -5,3 +5,5 @@ class Icons:
     def __init__(self):
         self.icon_dark_theme = ft.icons.DARK_MODE
         self.icon_light_theme = ft.icons.LIGHT_MODE
+        self.icon_move_right = ft.icons.CHEVRON_RIGHT_OUTLINED
+        self.icon_move_left = ft.icons.CHEVRON_LEFT_OUTLINED
